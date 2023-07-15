b3456b43ebd138c15ea21886a15206472d63ebae
Rachita5873 <76832946+Rachita5873@users.noreply.github.com>
2023-07-15 23:56:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 function fibonacci(num) {
-// your code here
+let num1 = 0;
+	let num2 = 1;
+	let numI;
+
+	for(let i=2; i<num; i++){
+		numI=num1+num2;
+		num1=num2;
+		num2=numI;
+	}
+
+	if(num==1){
+		return num1;
+	}
+
+	if(num==2){
+		return num2;
+	}
+
+
+	return numI;
+	
 }
 
 module.exports = fibonacci;
